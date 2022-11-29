ed5f47330f5690d91b696f89b5e99af0e394b9ef
mzemskov <mzemskov@dimax.digital>
2022-11-29 18:45:50 +0300

    completed 1st task + gitignore

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите 5ти значное число");
+int a = int.Parse(Console.ReadLine());
+if (a >= 1000 && a < 100000)
+{
+	if (a / 10000 == a % 10 && (a / 1000) % 10 == (a / 10) % 10)
+	Console.WriteLine("Да");
+	else
+	Console.WriteLine("Нет");
+}
+else
+Console.WriteLine("Вы ввели не 5ти значное число");
